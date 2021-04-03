f3db098108bb461e808d28d1f73c19041a636eb2
Kerry Lang <klang812@gmail.com>
2021-04-03 08:18:18 -0700

    add update shelter spec test PASS

--- spec/requests/update_shelters_spec.rb
+++ spec/requests/update_shelters_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-describe 'update a shelter' do
-  
+describe 'update a shelter', :type => :request do
+  let!(:shelter) { FactoryBot.create(:shelter) }
+  let!(:attr) { {location: 'Portland'} }
+
+  context 'when successful' do
+    before do
+      patch shelter_path(shelter.id), params: attr
+      @parse = JSON.parse(response.body)
+    end
+    it 'updates a shelter' do
+      attr.entries do |key, val|
+        it { expect(@parse[key]).to eq val }
+      end
+    end
+    it 'returns a value of 200' do
+      expect(response).to have_http_status :created 
+    end
+  end
+  context 'when unsuccessful' do
+    it 'return a value of 422' do
+      patch shelter_path(shelter.id), params: {location: ""}
+      expect(response).to have_http_status :unprocessable_entity
+    end
+  end
 end
