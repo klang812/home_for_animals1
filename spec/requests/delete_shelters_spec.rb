560c524d282f242423eaa0be9a2701a1efe0895e
Kerry Lang <klang812@gmail.com>
2021-04-03 08:26:06 -0700

    add delete for shelter_spec PASS

---
+++ spec/requests/delete_shelters_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Delete a dog', :type => :request do
+  let!(:shelter) { FactoryBot.create(:shelter) }
+
+  context 'when successful' do
+    it 'deletes a shelter' do
+      delete shelter_path(shelter.id)
+      expect(response).to have_http_status :no_content
+    end
+  end
+  context 'when unsuccessful' do
+    it 'return a 404' do
+      delete shelter_path(shelter.id + 1)
+      expect(response).to have_http_status :not_found
+    end
+  end
+end
