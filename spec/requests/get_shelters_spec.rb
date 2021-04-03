477a241d047db6fd89a20b14bd5d61ca2e363059
Kerry Lang <klang812@gmail.com>
2021-04-03 08:07:17 -0700

    add show spec test PASS

--- spec/requests/get_shelters_spec.rb
+++ spec/requests/get_shelters_spec.rb
@@ -15,4 +15,24 @@ describe 'Get request Shelters' do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe '#show', :type => :request do
+    let!(:shelter) { shelters.first }
+
+    context 'when successful' do
+      before do
+        get shelter_path(shelter)
+        @parse = JSON.parse(response.body)
+      end
+      it 'returns a shelter' do
+        expect(@parse['id']).to eq shelter.id
+      end
+      it { expect(response).to have_http_status :success }
+    end
+
+    context 'when unsuccessful' do 
+      before { get shelter_path(shelters.last.id + 1) }
+      it { expect(response).to have_http_status :not_found }
+    end
+  end
 end
