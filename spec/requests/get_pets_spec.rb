20fb17a5174b99eebbc89b139689b8859516918d
Kerry Lang <klang812@gmail.com>
2021-04-06 08:18:01 -0700

    add README with no routing

--- spec/requests/get_pets_spec.rb
+++ spec/requests/get_pets_spec.rb
@@ -22,27 +22,19 @@ describe 'Get all of the pets', :type => :request do
   end
 end
 
-# describe '#show', :type => :request do
+describe '#show', :type => :request do
 
-#   context 'when successful' do
-#     before do
-#       FactoryBot.create(:shelter) do |shelter|
-#         FactoryBot.create_list(:pet, 10, shelter_id: shelter.id)
-#         get api_v1_shelter_pets_path(shelter.id)
-#         @parse = JSON.parse(response.body)
-#       end
-#     end
-#     it 'returns a pet' do
-#       expect(@parse['shelter_id']).to eq shelter.id
-#     end
-#     it 'returns status code of 200' do
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-#   context 'when unsuccessful' do
-#     before { get api_v1_shelter_pets_path(shelters.pets.last.shelter_id + 1) }
-#     it 'returns pet not found' do
-#       expect(response).to have_http_status(:not_found)
-#     end
-#   end
+  context 'when successful' do
+    before do
+      @shelter = Shelter.create(location: "Portland")
+      @pet = Pet.create(name: "Eli", animal_type: "Dog", breed: "Lab", shelter_id: @shelter.id)
+        get api_v1_shelter_pet_path(@shelter.id, @pet.id)
+    end
+    it 'returns a pet' do
+      expect(JSON.parse(response.body)['name']).to eq ("Eli")
+    end
+    it 'returns status code of 200' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
